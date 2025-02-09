573a4951b0ad0b6aab59f02a24061d3e462d653d
BobbyLin23 <linzhangsheng23@gmail.com>
2025-02-09 22:57:02 +0800

    feat: add ui libraray

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+import path from 'node:path'
 import { sveltekit } from '@sveltejs/kit/vite'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [sveltekit(), tailwindcss()],
+  resolve: {
+    alias: {
+      $lib: path.resolve('./src/lib'),
+    },
+  },
 })
