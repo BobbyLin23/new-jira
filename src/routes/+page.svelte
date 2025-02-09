11e9bca856105055c81fd85f61dfd7f96445a45c
BobbyLin23 <linzhangsheng23@gmail.com>
2025-02-09 23:28:05 +0800

    feat: set default ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,23 @@
   import { Button } from '$lib/components/ui/button'
 </script>
 
-<div class='p-2 text-2xl'>
+<div class='p-2 text-2xl flex gap-4 items-center'>
   <Button>
-    Click
+    Primary
+  </Button>
+  <Button variant='destructive'>
+    Destructive
+  </Button>
+  <Button variant='secondary'>
+    Secondary
+  </Button>
+  <Button variant='muted'>
+    Muted
+  </Button>
+  <Button variant='outline'>
+    Outline
+  </Button>
+  <Button variant='teritrary'>
+    Teritrary
   </Button>
 </div>
